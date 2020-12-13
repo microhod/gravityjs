cf476f4533464e8a589887cf55775e1d520eca4c
Sam Hodgkinson <55709069+microhod@users.noreply.github.com>
2020-12-13 19:20:29 +0000

    add inputs

--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
 import Scene from "./scene.js"
 
-new Scene({
+var gravity = document.getElementById("input-gravity")
+var numBalls = document.getElementById("input-num-balls")
+var border = document.getElementById("input-border")
+var button = document.getElementById("button-new")
+
+
+var minBallRadius = 1
+var maxBallRadius = 1.5
+var scene = new Scene({
   canvasId: "canvas",
-  minBallRadius: 1,
-  maxBallRadius: 1.5,
-  numBalls: 1000,
-  gravity: 0.5,
-  border: false
+  minBallRadius: minBallRadius,
+  maxBallRadius: maxBallRadius,
+  numBalls: numBalls.value,
+  gravity: gravity.value,
+  border: border.checked
+})
+
+var updateScene = () => {
+  // increase gravity exponentially along the slider
+  scene.gravity = gravity.value == -5 ? 0 : 2 ** gravity.value
+  scene.border = border.checked
+}
+
+// reload when sliders have loaded
+window.addEventListener('load', updateScene)
+
+// update on slider changes
+gravity.addEventListener("change", updateScene)
+border.addEventListener("change", updateScene)
+
+// create new balls on button click
+button.addEventListener("click", () => {
+  scene.createBalls(minBallRadius, maxBallRadius, numBalls.value)
+  updateScene()
 })
